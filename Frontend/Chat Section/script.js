69a35d1dcc1cf107fcd12fabcdabe9cd4084e86f
Darshan Nikam <136769490+Darshannikam9767@users.noreply.github.com>
2026-04-05 22:11:59 +0530

    added route for announcement section

--- Frontend/Chat Section/script.js
+++ Frontend/Chat Section/script.js
@@ -85,3 +85,42 @@ function addMessage(text, className) {
 
     chatBody.scrollTop = chatBody.scrollHeight;
 }
+
+const BACKEND = "http://127.0.0.1:5000"
+const ANNOUNCEMENT = "/chat/announcement"
+async function loadAnnouncement() {
+    const list = document.getElementById('announcement-list');
+
+    try {
+        const response = await fetch(BACKEND + ANNOUNCEMENT, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await response.json()
+        if (data.status === "success" && response.ok && data.announcemnet_data.length > 0) {
+            list.innerHTML = "";
+            data.announcemnet_data.forEach(ann => {
+                const card = document.createElement('div');
+                card.className = "announcement-card-simple";
+
+                card.innerHTML = `<p class="simple-desc">${ann.a_description}</p>`;
+
+                list.appendChild(card);
+            });
+        } else {
+            list.innerHTML = "<li>No new announcements at this time.</li>";
+        }
+
+    } catch (error) {
+        console.error("Error fetching announcements:", error);
+        list.innerHTML = "<li>Failed to load announcements.</li>";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadAnnouncement();
+
+    document.getElementById("user-input").focus()
+});
